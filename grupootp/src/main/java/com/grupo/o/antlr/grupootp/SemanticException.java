fee979f9cf483f451a6b83f1f39048956da402d1
nicobasa <142696216+nicobasa@users.noreply.github.com>
2026-06-17 14:11:51 -0300

    Delete grupootp directory

--- grupootp/src/main/java/com/grupo/o/antlr/grupootp/SemanticException.java
+++
@@ -1,9 +0,0 @@
-package com.grupo.o.antlr.grupootp;
-
-public class SemanticException extends RuntimeException {
-    private static final long serialVersionUID = 1L;
-
-    public SemanticException(String message) {
-        super(message);
-    }
-}
